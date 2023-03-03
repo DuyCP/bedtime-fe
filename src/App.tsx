4351a9ceff6a1cadb3cfb6696360013ad1aa3608
DuyCP <duy@coderpush.com>
2023-03-03 16:05:29 +0700

    style: add a black overlay

--- src/App.tsx
+++ src/App.tsx
@@ -244,11 +244,16 @@ const App = () => {
         className='App'
         py={20}
         sx={{
+          position: 'relative',
           backgroundImage:
             // 'url("https://images.unsplash.com/photo-1528818955841-a7f1425131b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3RhcnJ5JTIwc2t5fGVufDB8fDB8fA%3D%3D&w=1000&q=80")',
             'url("https://images.pexels.com/photos/9474172/pexels-photo-9474172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
         }}
       >
+        <div
+        className='overlay'
+        ></div>
+
         <Title order={1} align='center' mb={40}>
           Bedtime Stories
         </Title>
